ddbad6f4b35506382e8dda24b7638b6ed815b050
Alberto Mitroi <alberto.mitroi@encora.com>
2024-11-04 14:09:10 +0200

    feat: add try catch block for migration, change to async

--- src/Services/Discount/Discount.Grpc/Data/Extentions.cs
+++ src/Services/Discount/Discount.Grpc/Data/Extentions.cs
@@ -4,11 +4,21 @@ namespace Discount.Grpc.Data
 {
     public static class Extentions
     {
-        public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+        public static async Task<IApplicationBuilder> UseMigration(this IApplicationBuilder app)
         {
             using var scope = app.ApplicationServices.CreateScope();
             using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
-            dbContext.Database.MigrateAsync();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<DiscountContext>>();
+
+            try
+            {
+                await dbContext.Database.MigrateAsync();
+                logger.LogInformation("Database migration applied successfully.");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while applying the database migration.");
+            }
 
             return app;
         }
