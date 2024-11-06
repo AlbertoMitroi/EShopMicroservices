09d177c16dc07eb7e0e2e47ad842aa85b25ccf3a
Alberto Mitroi <alberto.mitroi@encora.com>
2024-11-06 15:04:11 +0200

    feat: add applicationdbcontext and configurations for dbsets

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+        
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Customer>().Property(c => c.Name).IsRequired().HasMaxLength(100);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
