c1243391214e2698108f083fe8de30034dd40e67
Alberto Mitroi <alberto.mitroi@encora.com>
2024-11-11 17:15:53 +0200

    feat: add predefinited frontend files

--- src/WebApps/Shopping.Web/Models/Catalog/ProductModel.cs
+++ src/WebApps/Shopping.Web/Models/Catalog/ProductModel.cs
@@ -1,17 +1,16 @@
-﻿namespace Shopping.Web.Models.Catalog
-{
-    public class ProductModel
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; } = default!;
-        public List<string> Category { get; set; } = new();
-        public string Description { get; set; } = default!;
-        public string ImageFile { get; set; } = default!;
-        public decimal Price { get; set; }
-    }
+﻿namespace Shopping.Web.Models.Catalog;
 
-    //wrapper classes
-    public record GetProductsResponse(IEnumerable<ProductModel> Products);
-    public record GetProductByCategoryResponse(IEnumerable<ProductModel> Products);
-    public record GetProductByIdResponse(ProductModel Product);
+public class ProductModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public List<string> Category { get; set; } = new();
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public decimal Price { get; set; }
 }
+
+//wrapper classes
+public record GetProductsResponse(IEnumerable<ProductModel> Products);
+public record GetProductByCategoryResponse(IEnumerable<ProductModel> Products);
+public record GetProductByIdResponse(ProductModel Product);
--- src/WebApps/Shopping.Web/Services/IBasketService.cs
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -1,42 +1,41 @@
 ﻿using System.Net;
 
-namespace Shopping.Web.Services
+namespace Shopping.Web.Services;
+
+public interface IBasketService
 {
-    public interface IBasketService
+    [Get("/basket-service/basket/{userName}")]
+    Task<GetBasketResponse> GetBasket(string userName);
+
+    [Post("/basket-service/basket")]
+    Task<StoreBasketResponse> StoreBasket(StoreBasketRequest request);
+
+    [Delete("/basket-service/basket/{userName}")]
+    Task<DeleteBasketResponse> DeleteBasket(string userName);
+
+    [Post("/basket-service/basket/checkout")]
+    Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
+
+    public async Task<ShoppingCartModel> LoadUserBasket()
     {
-        [Get("/basket-service/basket/{userName}")]
-        Task<GetBasketResponse> GetBasket(string userName);
+        // Get Basket If Not Exist Create New Basket with Default Logged In User Name: swn
+        var userName = "swn";
+        ShoppingCartModel basket;
 
-        [Post("/basket-service/basket")]
-        Task<StoreBasketResponse> StoreBasket(StoreBasketRequest request);
-
-        [Delete("/basket-service/basket/{userName}")]
-        Task<DeleteBasketResponse> DeleteBasket(string userName);
-
-        [Post("/basket-service/basket/checkout")]
-        Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
-
-        public async Task<ShoppingCartModel> LoadUserBasket()
+        try
         {
-            // Get Basket If Not Exist Create New Basket with Default Logged In User Name: swn
-            var userName = "swn";
-            ShoppingCartModel basket;
-
-            try
-            {
-                var getBasketResponse = await GetBasket(userName);
-                basket = getBasketResponse.Cart;
-            }
-            catch (ApiException apiException) when (apiException.StatusCode == HttpStatusCode.NotFound)
-            {
-                basket = new ShoppingCartModel
-                {
-                    UserName = userName,
-                    Items = []
-                };
-            }
-
-            return basket;
+            var getBasketResponse = await GetBasket(userName);
+            basket = getBasketResponse.Cart;
         }
+        catch (ApiException apiException) when (apiException.StatusCode == HttpStatusCode.NotFound)
+        {
+            basket = new ShoppingCartModel
+            {
+                UserName = userName,
+                Items = []
+            };
+        }
+
+        return basket;
     }
 }
