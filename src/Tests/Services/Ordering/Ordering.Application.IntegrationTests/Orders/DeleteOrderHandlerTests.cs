1e34c58734e0e4bb0df61b9ddb54f6d59a4b9af4
Alberto Mitroi <alberto.mitroi@encora.com>
2024-11-14 16:06:42 +0200

    feat: add create delete unit test

---
+++ src/Tests/Services/Ordering/Ordering.Application.IntegrationTests/Orders/DeleteOrderHandlerTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Ordering.Application.Orders.Commands.DeleteOrder;
+using Ordering.Shared;
+using Xunit;
+
+namespace Ordering.API.IntegrationTests.Orders
+{
+    public class DeleteOrderHandlerTests : BaseIntegrationTest
+    {
+        public DeleteOrderHandlerTests(IntegrationTestWebAppFactory factory)
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Handler_ShouldDelete_ExistingOrderWithGuid()
+        {
+            // Arrange 
+            var postOrderResult = await HelperClass.PostNewOrder(_httpClient);
+
+            // Act
+            var result = await Sender.Send(new DeleteOrderCommand(postOrderResult));
+
+            // Assert 
+            result.IsSuccess.Should().Be(true);
+        }
+    }
+}
