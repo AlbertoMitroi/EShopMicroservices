da965d81fbb56fa993c148459841ed53c8f768e9
Alberto Mitroi <alberto.mitroi@encora.com>
2024-11-14 16:07:19 +0200

    feat: add delete order integration test

---
+++ src/Tests/Services/Ordering/Ordering.API.IntegrationTest/DeleteOrderTests.cs
@@ -0,0 +1,62 @@
+using FluentAssertions;
+using Ordering.API.Endpoints;
+using Ordering.Shared;
+using System.Net;
+using System.Net.Http.Json;
+using Xunit;
+
+namespace Ordering.API.IntegrationTest
+{
+    public class DeleteOrderTests : BaseIntegrationTest
+    {
+        public DeleteOrderTests(IntegrationTestWebAppFactory factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task DeleteOrder_ShouldReturn200Ok_WhenOrderExists()
+        {
+            // Arrange
+            Guid orderId = await HelperClass.PostNewOrder(_httpClient);
+
+            // Act
+            var deleteResponse = await _httpClient.DeleteAsync($"/orders/{orderId}");
+            deleteResponse.EnsureSuccessStatusCode();
+
+            var responseBody = await deleteResponse.Content.ReadFromJsonAsync<DeleteOrderResponse>();
+
+            // Assert
+            deleteResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            responseBody.Should().NotBeNull();
+            responseBody!.IsSuccess.Should().BeTrue();
+
+            var deletedOrder = await _context.Orders.FindAsync(orderId);
+            deletedOrder.Should().BeNull();
+        }
+        [Fact]
+        public async Task DeleteOrder_ShouldReturn404NotFound_WhenOrderDoesNotExist()
+        {
+            // Arrange
+            Guid nonExistentOrderId = Guid.NewGuid();
+
+            // Act
+            var deleteResponse = await _httpClient.DeleteAsync($"/orders/{nonExistentOrderId}");
+
+            // Assert
+            deleteResponse.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task DeleteOrder_ShouldReturn400BadRequest_WhenIdIsInvalid()
+        {
+            // Arrange
+            Guid invalidOrderId = Guid.NewGuid();
+
+            // Act
+            var deleteResponse = await _httpClient.DeleteAsync($"/orders/{invalidOrderId}");
+
+            // Assert
+            deleteResponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
