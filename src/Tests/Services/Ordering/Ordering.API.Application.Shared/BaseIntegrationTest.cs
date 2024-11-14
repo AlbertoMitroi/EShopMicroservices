7bcf287923f441df30e6af029edc55b4781dae80
Alberto Mitroi <alberto.mitroi@encora.com>
2024-11-14 16:07:56 +0200

    feat: add all projects for tests

---
+++ src/Tests/Services/Ordering/Ordering.API.Application.Shared/BaseIntegrationTest.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
+using Xunit;
+
+namespace Ordering.Shared
+{
+    public abstract class BaseIntegrationTest : IClassFixture<IntegrationTestWebAppFactory>
+    {
+        private readonly IServiceScope _scope;
+        protected readonly ISender Sender;
+        protected readonly ApplicationDbContext _context;
+        private readonly IntegrationTestWebAppFactory factory;
+        protected readonly HttpClient _httpClient;
+
+        protected BaseIntegrationTest(IntegrationTestWebAppFactory factory)
+        {
+            _scope = factory.Services.CreateScope();
+
+            Sender = _scope.ServiceProvider.GetRequiredService<ISender>();
+
+            _context = _scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            _httpClient = factory.CreateClient();
+
+            this.factory = factory;
+        }
+
+        protected IServiceScope CreateScope()
+        {
+            return factory.Services.CreateScope();
+        }
+    }
+}
---
+++ src/Tests/Services/Ordering/Ordering.API.Application.Shared/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
+using Testcontainers.MsSql;
+using Xunit;
+
+namespace Ordering.Shared
+{
+    public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+
+        private readonly MsSqlContainer _sqlContainer;
+        public IntegrationTestWebAppFactory()
+        {
+            _sqlContainer = new MsSqlBuilder()
+                .WithImage("mcr.microsoft.com/mssql/server:2022-CU15-GDR1-ubuntu-22.04")
+                .WithExposedPort("1435")
+                .Build();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    var connectionString = _sqlContainer.GetConnectionString();
+                    options.UseSqlServer(connectionString);
+                });
+
+                //var serviceProvider = services.BuildServiceProvider();
+                //using (var scope = serviceProvider.CreateScope())
+                //{
+                //    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                //    db.Database.EnsureCreated();
+                //}
+            });
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _sqlContainer.StartAsync();
+        }
+        async Task IAsyncLifetime.DisposeAsync()
+        {
+            await _sqlContainer.StopAsync();
+        }
+    }
+}
